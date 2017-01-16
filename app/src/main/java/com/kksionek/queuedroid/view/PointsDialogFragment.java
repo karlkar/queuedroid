dd3daf0d3f55dfc9b310e89d1e3cffdf83610c82
Karol Ksionek <karlkar@tlen.pl>
2017-01-16 20:31:41 +0100

    Changed behavior of input points dialog

--- app/src/main/java/com/kksionek/queuedroid/view/PointsDialogFragment.java
+++ app/src/main/java/com/kksionek/queuedroid/view/PointsDialogFragment.java
@@ -9,8 +9,12 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kksionek.queuedroid.R;
 
@@ -41,24 +45,45 @@ public class PointsDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         final EditText input = new EditText(getActivity());
         input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        input.setLines(1);
+        input.setSingleLine();
+        input.setImeOptions(EditorInfo.IME_ACTION_DONE);
         builder.setView(input);
         builder.setMessage(R.string.view_keyboard_points_collected)
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        int points = Integer.parseInt(input.getText().toString());
-                        if (mListener != null)
-                            mListener.onDialogPositiveClick(points);
-                        mInputMethodManager.hideSoftInputFromWindow(input.getWindowToken(), 0);
+                        acceptInput(dialog, input);
                     }
                 })
                 .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         mInputMethodManager.hideSoftInputFromWindow(input.getWindowToken(), 0);
+                        dialog.dismiss();
                     }
                 });
-
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                acceptInput(alertDialog, input);
+                return true;
+            }
+        });
         input.requestFocus();
         mInputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-        return builder.create();
+        return alertDialog;
+    }
+
+    private void acceptInput(DialogInterface dialog, EditText input) {
+        if (input.getText().toString().isEmpty()) {
+            Toast.makeText(getContext(), R.string.view_keyboard_input_points_toast, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        int points = Integer.parseInt(input.getText().toString());
+        if (mListener != null)
+            mListener.onDialogPositiveClick(points);
+        mInputMethodManager.hideSoftInputFromWindow(input.getWindowToken(), 0);
+        dialog.dismiss();
     }
 }
